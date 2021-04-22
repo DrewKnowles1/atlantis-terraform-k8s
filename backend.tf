6816b45fc3581fb8b2ec73535f022de8efd51962
Andrew Knowles <andrew.knowles@control-plane.io>
2021-04-22 15:43:46 +0100

    Removing creds line alltogether

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket  = "atlantis-tf-state-dev-cp"
-    prefix  = "terraform/state"
-    credentials = "/home/aknow/tf/clean-patrol-311410-0a2251d78e02.json"
-  }
-}
-
-
