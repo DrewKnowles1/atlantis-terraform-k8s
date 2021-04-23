e78819840413138bb3bf08edb8b11e9e15fedef0
Andrew Knowles <andrew.knowles@control-plane.io>
2021-04-23 13:27:41 +0100

    Switching repeated values to tf vars

--- cluster-terraform/variables.tf
+++ cluster-terraform/variables.tf
@@ -1,1 +1,19 @@
-#
+variable "project_id" {
+  type = string
+  default = "clean-patrol-311410"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west2"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-west2-b"
+}
+
+variable "machine_type" {
+  type = string
+  default = "n2d-standard-2"
+}
